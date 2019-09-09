cd18ad06df9bc66afd4593f7ac794b60df23863d
XZhang <quantsleeper@gmail.com>
2019-09-09 17:37:00 +0800

    add: basic msg

---
+++ 4_msg.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    
+    def initUI(self):
+        self.setGeometry(400, 400, 400, 400)
+        self.setWindowTitle('msg')
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', 'Are you sure to quit?', 
+                QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
